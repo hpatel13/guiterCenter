3f352ab7d832ad51ab58a4b60291727234e6a2b1
harsh <hpatel13@calstatela.edu>
2015-10-01 11:18:54 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guitercenterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the guitercenterApp
+ */
+angular.module('guitercenterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
